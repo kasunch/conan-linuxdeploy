62fbad848629ec6f7d514aa8c3f7de172529a47a
Alexis Lopez Zubieta <contact@azubieta.net>
2019-04-30 11:53:06 -0500

    Add missing dependencies

--- build.py
+++ build.py
@@ -1,7 +1,8 @@
 from cpt.packager import ConanMultiPackager
 
-
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
-    builder.add_common_builds(shared_option_name="linuxdeploy:shared")
+    remotes = [("https://api.bintray.com/conan/appimage-conan-community/public-conan", "true", "appimage"),
+               ("https://api.bintray.com/conan/bincrafters/public-conan", "true", "bincrafters")]
+    builder = ConanMultiPackager(remotes=remotes, build_policy="missing")
+    builder.add_common_builds()
     builder.run()
