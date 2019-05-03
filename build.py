7a50530f1d6bf82711033b8b3ceb47faf1cc6cb5
Alexis Lopez Zubieta <contact@azubieta.net>
2019-05-03 17:00:49 -0500

    Remove libstdc++11 requirement

--- build.py
+++ build.py
@@ -7,9 +7,4 @@ if __name__ == "__main__":
     builder = ConanMultiPackager(remotes=remotes, build_policy="outdated")
     builder.add_common_builds()
 
-    # libstc++11 is required by gtest therefore by the whole build
-    for settings, options, env_vars, build_requires, reference in builder.items:
-        settings["compiler.libcxx"] = 'libstdc++11'
-        settings["cppstd"] = '11'
-
     builder.run()
